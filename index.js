8fc3dc160b8e7b4a145a06a1b6daacb33a1dadf7
Manel <quemonmanel@gmail.com>
2023-04-26 21:48:54 +0200

    chore:starting with mongoDB

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const dotenv = require("dotenv").config(); 
 const PORT = process.env.PORT || 3000; 
 
+app.use("/", (req, res) => {
+    res.send("Hello! I am your server");
+});
+
 app.listen(PORT, () => {
     console.log(`Running at PORT ${PORT}`); 
 }); 
