13742efe0969855399bc831748ef72ccfca8e956
Manel <quemonmanel@gmail.com>
2023-04-28 21:00:39 +0200

    feat:register

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const db = require("./config/dB");
 const app = express(); 
 const dotenv = require("dotenv").config(); 
 const PORT = process.env.PORT || 3000; 
-const authRouter = require("./routes/authRoute")
+const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
 db();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use("/", (req, res) => {
     res.send("Hello! I am your server");
 });
-
 app.use("/api/user",authRouter); 
 app.listen(PORT, () => {
     console.log(`Running at PORT ${PORT}`); 
