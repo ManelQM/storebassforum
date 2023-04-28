a1da6581d825906b5f5698966084bbdd40720143
Manel <quemonmanel@gmail.com>
2023-04-28 19:11:29 +0200

    feat:first model with mongoDB

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const db = require("./config/dB");
 const app = express(); 
 const dotenv = require("dotenv").config(); 
 const PORT = process.env.PORT || 3000; 
+const authRouter = require("./routes/authRoute")
 db();
 
 app.use("/", (req, res) => {
     res.send("Hello! I am your server");
 });
 
+app.use("/api/user",authRouter); 
 app.listen(PORT, () => {
     console.log(`Running at PORT ${PORT}`); 
 }); 
